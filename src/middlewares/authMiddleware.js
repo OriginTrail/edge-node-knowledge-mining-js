99847714ea399ea3347b0ea26b92a6c7e8e5708f
stenjo1 <stefanija.mar1999@gmail.com>
2025-05-14 15:20:58 +0200

    fix route

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,20 +2,9 @@ const axios = require("axios");
 const { transformUserData } = require("../utils/utils");
 
 const baseUserData = {
-  name: "John",
-  endpoint: process.env.OT_NODE_HOSTNAME,
-  blockchain: {
-    name: "otp:2043",
-  },
-  vectorDBUri: process.env.VECTOR_DB_URI,
-  vectorDBUsername: process.env.VECTOR_DB_USERNAME,
-  vectorDBPassword: process.env.VECTOR_DB_PASSWORD,
-  embeddingModelAPIKey: process.env.EMBEDDING_MODEL_API_KEY,
-  embeddingModel: process.env.EMBEDDING_MODEL,
   provider: "openai",
   model: "gpt-4o-mini",
   apiKey: process.env.OPEN_AI_KEY,
-  cohereKey: process.env.COHERE_KEY,
 };
 
 exports.authMiddleware = async (req, res, next) => {
@@ -30,7 +19,7 @@ exports.authMiddleware = async (req, res, next) => {
     }
 
     const authResponse = await axios.get(
-      `${process.env.AUTH_SERVICE_ENDPOINT}/auth/check`,
+      `${process.env.AUTH_SERVICE_ENDPOINT}/check`,
       {
         headers: {
           Cookie: sessionCookie,
